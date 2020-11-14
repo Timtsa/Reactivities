bee63d3e55b462d75b18957118243adeadcc90ab
Yura Temtsunik <timtsa85@gmail.com>
2020-11-14 19:18:59 +0200

    comit 1

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,14 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy",policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         
         }
@@ -47,7 +55,8 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+          //  app.UseAuthorization();
+          app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
